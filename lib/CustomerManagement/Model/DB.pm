142983fc45cccf598f9cdec5d8ce3ac9a5185d9e
samgunathillake <134361207+samgunathillake@users.noreply.github.com>
2024-06-12 16:23:37 +0530

    Update DB.pm
    code cleanup

--- lib/CustomerManagement/Model/DB.pm
+++ lib/CustomerManagement/Model/DB.pm
@@ -2,13 +2,12 @@ package CustomerManagement::Model::DB;
 use base 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->config(
-	    schema_class => 'CustomerManagement::Schema',
-	        connect_info => {
-			        dsn => 'dbi:SQLite:myapp.db',
-				        user => '',
-					        password => '',
-						    }
-					    );
+    schema_class => 'CustomerManagement::Schema',
+    connect_info => {
+        dsn => 'dbi:SQLite:myapp.db',
+        user => '',
+        password => '',
+    }
+);
 
-					    1;
-
+1;
